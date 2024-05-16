f589f706128498605fab2edededbfbe71ce2d05f
Masayasu Yamamoto <491754+msysyamamoto@users.noreply.github.com>
2024-05-16 13:24:05 +0000

    Refactor main.rs for improved readability and maintainability

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,125 @@
-use std::io::{self, Write};
-use std::{thread, time};
+use std::{
+    io::{self, Write},
+    thread, time,
+};
 
-const WIDTH: i32 = 10;
-const HEIGHT: i32 = 10;
-const SLEEP_MILLIS: u64 = 200;
+const WIDTH: i32 = 29;
+const HEIGHT: i32 = 11;
+const SLEEP_MILLIS: u64 = 50;
 
 type Pos = (i32, i32);
 
+#[derive(Debug, PartialEq, Eq)]
 struct Board {
     cells: Vec<Pos>,
+    width: i32,
+    height: i32,
 }
 
 impl Board {
-    pub fn new(args: &[(i32, i32)]) -> Self {
-        let cells = args.iter().map(|(x, y)| (*x, *y)).collect::<Vec<_>>();
-        Self { cells }
-    }
-
-    pub fn from_vec(cells: &Vec<Pos>) -> Self {
+    fn new(width: i32, height: i32, cells: &Vec<Pos>) -> Self {
         Self {
             cells: cells.clone(),
+            width,
+            height,
         }
+    }
+
+    fn is_extinct(&self) -> bool {
+        self.cells.is_empty()
+    }
+
+    fn next_gen(&self) -> Board {
+        let mut survivors = self.survivors();
+        let births = self.births();
+        survivors.extend(births);
+        Self::new(self.width, self.height, &survivors)
+    }
+
+    fn survivors(&self) -> Vec<Pos> {
+        self.cells
+            .iter()
+            .filter(|pos| {
+                let count = self.live_neighbs(pos);
+                count == 2 || count == 3
+            })
+            .map(|(x, y)| (*x, *y))
+            .collect()
+    }
+
+    fn live_neighbs(&self, pos: &Pos) -> i32 {
+        self.neighbs(pos)
+            .into_iter()
+            .filter(|p| self.is_alive(p))
+            .count() as i32
+    }
+
+    fn is_alive(&self, pos: &Pos) -> bool {
+        self.cells.contains(pos)
+    }
+
+    fn is_empty(&self, pos: &Pos) -> bool {
+        !self.is_alive(pos)
+    }
+
+    fn births(&self) -> Vec<Pos> {
+        let mut new_cells = vec![];
+        for x in 1..=self.width {
+            for y in 1..=self.height {
+                let pos = (x, y);
+                if self.is_empty(&pos) && self.live_neighbs(&pos) == 3 {
+                    new_cells.push(pos);
+                }
+            }
+        }
+        new_cells
+    }
+
+    fn neighbs(&self, pos: &Pos) -> Vec<Pos> {
+        let (x, y) = (pos.0, pos.1);
+        vec![
+            (x - 1, y - 1),
+            (x, y - 1),
+            (x + 1, y - 1),
+            (x - 1, y),
+            (x + 1, y),
+            (x - 1, y + 1),
+            (x, y + 1),
+            (x + 1, y + 1),
+        ]
+        .into_iter()
+        .map(|pos| self.wrap(pos))
+        .collect()
+    }
+
+    fn wrap(&self, (x, y): (i32, i32)) -> Pos {
+        (
+            (x - 1).rem_euclid(self.width) + 1,
+            (y - 1).rem_euclid(self.height) + 1,
+        )
     }
 }
 
 fn main() {
     let sleep_duration = time::Duration::from_millis(SLEEP_MILLIS);
-    let mut board = Board::new(&[(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)]);
+    let mut board = glider(WIDTH, HEIGHT);
 
-    for _ in 0..50 {
+    loop {
         cls();
         show_cells(&board);
-        board = next_gen(&board);
+        if board.is_extinct() {
+            break;
+        }
+
+        board = board.next_gen();
         goto(&(WIDTH + 1, HEIGHT + 1));
         io::stdout().flush().unwrap();
         thread::sleep(sleep_duration);
     }
 }
 
-fn next_gen(board: &Board) -> Board {
-    let mut survivors = survivors(board);
-    let births = births(board);
-    survivors.extend(births);
-
-    Board::from_vec(&survivors)
-}
-
-fn births(board: &Board) -> Vec<Pos> {
-    let mut new_cells = vec![];
-    for x in 1..=WIDTH {
-        for y in 1..=HEIGHT {
-            let pos = (x, y);
-            if is_empty(board, &pos) && live_neighbs(board, &pos) == 3 {
-                new_cells.push(pos);
-            }
-        }
-    }
-    new_cells
-}
-
-fn survivors(board: &Board) -> Vec<Pos> {
-    board
-        .cells
-        .iter()
-        .filter(|pos| {
-            let count = live_neighbs(board, pos);
-            count == 2 || count == 3
-        })
-        .map(|pos| (pos.0, pos.1))
-        .collect()
-}
-
-fn live_neighbs(board: &Board, pos: &Pos) -> i32 {
-    neighbs(pos)
-        .into_iter()
-        .filter(|p| is_alive(&board, p))
-        .count() as i32
-}
-
-fn neighbs(pos: &Pos) -> Vec<Pos> {
-    let (x, y) = (pos.0, pos.1);
-    vec![
-        (x - 1, y - 1),
-        (x, y - 1),
-        (x + 1, y - 1),
-        (x - 1, y),
-        (x + 1, y),
-        (x - 1, y + 1),
-        (x, y + 1),
-        (x + 1, y + 1),
-    ]
-    .into_iter()
-    .map(|pos| wrap(pos))
-    .collect()
-}
-
-fn wrap(pos: (i32, i32)) -> Pos {
-    let (x, y) = (pos.0, pos.1);
-    (
-        (x - 1).rem_euclid(WIDTH) + 1,
-        (y - 1).rem_euclid(HEIGHT) + 1,
-    )
-}
-
-fn is_alive(board: &Board, pos: &Pos) -> bool {
-    board.cells.contains(pos)
-}
-
-fn is_empty(board: &Board, pos: &Pos) -> bool {
-    !is_alive(&board, pos)
+fn glider(width: i32, height: i32) -> Board {
+    Board::new(width, height, &vec![(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)])
 }
 
 fn show_cells(board: &Board) {
@@ -122,8 +133,8 @@ fn write_at(pos: &Pos, c: char) {
     print!("{}", c);
 }
 
-fn goto(pos: &Pos) {
-    print!("\x1b[{};{}H", pos.1, pos.0);
+fn goto((x, y): &Pos) {
+    print!("\x1b[{};{}H", *y, *x);
 }
 
 fn cls() {
@@ -132,68 +143,28 @@ fn cls() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{neighbs, wrap, HEIGHT, WIDTH};
+    use super::*;
 
     #[test]
-    fn test_neighbs() {
+    fn test_board_next_gen() {
+        let w = 5;
+        let h = 5;
         let tests = vec![
             (
-                (2, 2),
-                vec![
-                    (1, 1),
-                    (2, 1),
-                    (3, 1),
-                    (1, 2),
-                    (3, 2),
-                    (1, 3),
-                    (2, 3),
-                    (3, 3),
-                ],
+                glider(w, h),
+                Board::new(w, h, &vec![(4, 3), (3, 4), (4, 4), (3, 2), (5, 3)]),
             ),
             (
-                (1, 1),
-                vec![
-                    (WIDTH, HEIGHT),
-                    (1, HEIGHT),
-                    (2, HEIGHT),
-                    (WIDTH, 1),
-                    (2, 1),
-                    (WIDTH, 2),
-                    (1, 2),
-                    (2, 2),
-                ],
+                Board::new(w, h, &vec![(5, 4), (4, 5), (5, 5), (1, 4), (4, 3)]),
+                Board::new(w, h, &vec![(4, 5), (5, 5), (1, 4), (1, 5), (5, 3)]),
             ),
             (
-                (WIDTH, HEIGHT),
-                vec![
-                    (WIDTH - 1, HEIGHT - 1),
-                    (WIDTH, HEIGHT - 1),
-                    (1, HEIGHT - 1),
-                    (WIDTH - 1, HEIGHT),
-                    (1, HEIGHT),
-                    (WIDTH - 1, 1),
-                    (WIDTH, 1),
-                    (1, 1),
-                ],
+                Board::new(w, h, &vec![(4, 5), (5, 5), (1, 4), (1, 5), (5, 3)]),
+                Board::new(w, h, &vec![(5, 5), (1, 4), (1, 5), (4, 4), (5, 1)]),
             ),
         ];
-
-        for (pos, expect) in tests {
-            assert_eq!(neighbs(&pos), expect);
-        }
-    }
-
-    #[test]
-    fn test_wrap() {
-        let tests = vec![
-            ((WIDTH, HEIGHT), (WIDTH, HEIGHT)),
-            ((1, 1), (1, 1)),
-            ((0, 0), (WIDTH, HEIGHT)),
-            ((WIDTH + 1, HEIGHT + 1), (1, 1)),
-        ];
-
-        for (pos, expect) in tests {
-            assert_eq!(wrap(pos), expect);
+        for (board, expect) in tests {
+            assert_eq!(board.next_gen(), expect);
         }
     }
 }
